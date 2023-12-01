d744d074b4bf38a3a37ad6dbd1f98cffb73de4a1
Jansoon <57699212+ckdals4600@users.noreply.github.com>
2023-12-01 12:58:22 +0900

    Revert "2 feature/add store in specific area"

--- src/main/java/umc/spring/service/store/StoreService.java
+++
@@ -1,9 +0,0 @@
-package umc.spring.service.store;
-
-
-import umc.spring.domain.dto.request.AddStoreReq;
-import umc.spring.domain.dto.response.AddStoreRes;
-
-public interface StoreService {
-    AddStoreRes add(AddStoreReq req);
-}
